f1617d9f6679201f342523368b48fa9fd6b98ba5
François D <francois.delfort@keycoopt.com>
2021-12-03 18:27:25 +0100

    Day 2 - Part 2

--- day1.rb
+++ day1.rb
@@ -1,20 +1,38 @@
 require_relative "spec_helper"
 
-def count_increases(string)
+def count_increases(nums)
   count = 0
   prev = nil
-  string.split("\n").map(&:to_i).each do |num|
+  nums.each do |num|
     count += 1 if prev && prev < num
     prev = num
   end
   count
 end
 
-# puts count_increases(File.read("day1_input.txt"))
+def sum_by_window(nums)
+  sums = []
+  n_minus_1 = n_minus_2 = nil
+  nums.each do |num|
+    if n_minus_1 && n_minus_2
+      sums << num + n_minus_1 + n_minus_2
+    end
+    n_minus_2 = n_minus_1
+    n_minus_1 = num
+  end
+  sums
+end
 
-RSpec.describe "#count_increases" do
-  it "works" do
-    input = <<~INPUT
+def read_nums_from_string(string)
+  string.split("\n").map(&:to_i)
+end
+
+# puts count_increases(File.read("day1_input.txt"))
+# puts count_increases(sum_by_window(read_nums_from_string(File.read("day1_input.txt"))))
+
+RSpec.describe "Day 1" do
+  let(:example) do
+    <<~INPUT
       199
       200
       208
@@ -26,10 +44,27 @@ RSpec.describe "#count_increases" do
       260
       263
     INPUT
-    expect(count_increases(input)).to eql 7
   end
 
-  it "works #2" do
-    expect(count_increases(File.read("day1_input.txt"))).to eql 1624
+  describe "#sum_by_window" do
+    it "works" do
+      expect(sum_by_window([1, 2, 3, 4])).to eql [1 + 2 + 3, 2 + 3 + 4]
+    end
+  end
+
+  specify "part 1 - example" do
+    expect(count_increases(read_nums_from_string(example))).to eql 7
+  end
+
+  specify "part 1 - answer" do
+    expect(count_increases(read_nums_from_string(File.read("day1_input.txt")))).to eql 1624
+  end
+
+  specify "part 2 - example" do
+    expect(count_increases(sum_by_window(read_nums_from_string(example)))).to eql 5
+  end
+
+  specify "part 2 - answer" do
+    expect(count_increases(sum_by_window(read_nums_from_string(File.read("day1_input.txt"))))).to eql 1653
   end
 end
