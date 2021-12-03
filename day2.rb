82530828912c661bf98f0391dabe93680aab2f9f
François D <francois.delfort@keycoopt.com>
2021-12-03 18:45:34 +0100

    Day 2 - Part 2

--- day2.rb
+++ day2.rb
@@ -1,24 +1,25 @@
 require_relative "spec_helper"
 
-Coords = Struct.new(:depth, :horizontal_position)
+Coords = Struct.new(:depth, :horizontal_position, :aim)
 
 def compute_coords(commands)
-  coords = Coords.new(0, 0)
+  coords = Coords.new(0, 0, 0)
   commands.each do |command|
     order, unit = command.split(" ")
     unit = unit.to_i
     if order == "forward"
       coords.horizontal_position += unit
+      coords.depth += coords.aim * unit
     elsif order == "down"
-      coords.depth += unit
+      coords.aim += unit
     elsif order == "up"
-      coords.depth -= unit
+      coords.aim -= unit
     end
   end
   coords
 end
 
-RSpec.describe "Day 1" do
+RSpec.describe "Day 2" do
   let(:example) do
     <<~INPUT
       forward 5
@@ -30,17 +31,30 @@ RSpec.describe "Day 1" do
     INPUT
   end
 
-  specify "part 1 - example" do
+  skip "part 1 - example" do
     coords = compute_coords(example.split("\n"))
     expect(coords.depth).to eql 10
     expect(coords.horizontal_position).to eql 15
   end
 
-  specify "part 1 - answer" do
+  skip "part 1 - answer" do
     coords = compute_coords(File.read("day2_input.txt").split("\n"))
     expect(coords.depth).to eql 1091
     expect(coords.horizontal_position).to eql 1927
     expect(coords.depth * coords.horizontal_position).to eql 1091 * 1927 # 2102357
+  end
+
+  specify "part 2 - example" do
+    coords = compute_coords(example.split("\n"))
+    expect(coords.depth).to eql 60
+    expect(coords.horizontal_position).to eql 15
+  end
+
+  specify "part 2 - answer" do
+    coords = compute_coords(File.read("day2_input.txt").split("\n"))
+    expect(coords.depth).to eql 1090312
+    expect(coords.horizontal_position).to eql 1927
+    expect(coords.depth * coords.horizontal_position).to eql 1090312 * 1927 # 2102357
   end
 end
 
