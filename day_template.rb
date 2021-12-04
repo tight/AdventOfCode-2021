d969aac85b802181dc7f2fae3cb97da009c5a646
François D <francois.delfort@keycoopt.com>
2021-12-04 10:58:27 +0100

    Add day template

---
+++ day_template.rb
@@ -0,0 +1,24 @@
+require_relative "spec_helper"
+
+public
+
+RSpec.describe "Day XXX" do
+  let(:example) do
+    <<~INPUT
+    INPUT
+      .split("\n")
+  end
+  let(:input) { File.read("dayXXX_input.txt").split("\n") }
+
+  specify "part 1 - example" do
+  end
+
+  skip "part 1 - answer" do
+  end
+
+  skip "part 2 - example" do
+  end
+
+  skip "part 2 - answer" do
+  end
+end
