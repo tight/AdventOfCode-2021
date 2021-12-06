ac2be07829313885969bb47f908653ded720860d
François D <francois.delfort@keycoopt.com>
2021-12-06 13:10:04 +0100

    Day 6 - Part 2

--- day6.rb
+++ day6.rb
@@ -1,24 +1,35 @@
 require_relative "spec_helper"
 
 class Shoal
-  attr_reader :states
+  attr_reader :counts
 
   def initialize(states)
-    @states = states
+    @counts = states.tally
   end
 
   def wait(days = 1)
     days.times do
-      @states = states.flat_map do |state|
-        state == 0 ? [6, 8] : state - 1
+      new_counts = Hash.new(0)
+      counts.each do |state, count|
+        if state == 0
+          new_counts[8] += count
+          new_counts[6] += count
+        else
+          new_counts[state - 1] += count
+        end
       end
+      @counts = new_counts
     end
     self
   end
 
   def count
-    states.count
+    counts.values.sum
   end
+
+  private
+
+  attr_reader :counts
 end
 
 RSpec.describe "Day 6" do
@@ -29,11 +40,32 @@ RSpec.describe "Day 6" do
     nums.split(",").map(&:to_i)
   end
 
-  specify "states" do
-    expect(Shoal.new([5]).wait.states).to eql [4]
-    expect(Shoal.new([0]).wait.states).to eql [6, 8]
-    expect(Shoal.new([5]).wait(2).states).to eql [3]
-    expect(Shoal.new([0]).wait(2).states).to eql [5, 7]
+  describe "computation" do
+    specify "simple" do
+      s = Shoal.new([5]).wait
+      expect(s.count).to eql 1
+    end
+
+    specify "birth" do
+      s = Shoal.new([0]).wait
+      expect(s.count).to eql 2
+    end
+
+    specify "several days - simple" do
+      s = Shoal.new([5]).wait(2)
+      expect(s.count).to eql 1
+    end
+
+    specify "several days - birth" do
+      s = Shoal.new([0]).wait(2)
+      expect(s.count).to eql 2
+    end
+
+    specify "severy days - severy births" do
+      expect(Shoal.new([0]).wait.count).to eql 2
+      expect(Shoal.new([0]).wait(8).count).to eql 3
+      expect(Shoal.new([0]).wait(10).count).to eql 4
+    end
   end
 
   specify "part 1 - example" do
@@ -47,9 +79,13 @@ RSpec.describe "Day 6" do
     expect(s.wait(80).count).to eql 350149
   end
 
-  skip "part 2 - example" do
+  specify "part 2 - example" do
+    s = Shoal.new(example)
+    expect(s.wait(256).count).to eql 26_984_457_539
   end
 
-  skip "part 2 - answer" do
+  specify "part 2 - answer" do
+    s = Shoal.new(input)
+    expect(s.wait(256).count).to eql 1590327954513
   end
 end
