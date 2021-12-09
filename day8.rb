f7cd2a8cdbe00e82e386f3509ff402d7c384667c
François D <francois.delfort@keycoopt.com>
2021-12-09 21:06:10 +0100

    Day 8 - Part 2

--- day8.rb
+++ day8.rb
@@ -5,23 +5,32 @@ class Display
 
   def initialize(lines)
     @lines = lines.map { |line| line.scan(/[a-g]+/) }
-    @out = lines.map { _1[-4..] }
+    @out = @lines.map { _1[-4..] }
   end
 
   def easy_digit_count
-    # digit => segment used
-    # 1     => 2
-    # 4     => 4
-    # 7     => 3
-    # 8     => 7
     out.flatten.count { |digit| [2, 4, 3, 7].include?(digit.length) }
   end
 
-  # *acedgfb* cdfbe gcdfa fbcad *dab* cefabd cdfgeb *eafb* cagedb *ab* | cdfeb fcadb cdfeb cdbaf
-  # 2 => ab
-  # 4 => eafb
-  # 7 => dab
-  # 8 => acedgfb
+  LETTERS_TO_NUMBERS = {
+    "abcefg" => 0,
+    "cf" => 1,
+    "acdeg" => 2,
+    "acdfg" => 3,
+    "bcdf" => 4,
+    "abdfg" => 5,
+    "abdefg" => 6,
+    "acf" => 7,
+    "abcdefg" => 8,
+    "abcdfg" => 9,
+  }
+  VALID_COMBINATIONS = LETTERS_TO_NUMBERS.keys
+
+  def output_sum
+    lines.map
+      output(digits)
+    end.sum
+  end
 
   #   0:      1:      2:      3:      4:
   #  aaaa    ....    aaaa    aaaa    ....
@@ -41,83 +50,31 @@ class Display
   # .    f  e    f  .    f  e    f  .    f
   #  gggg    gggg    ....    gggg    gggg
 
-  # using 2
+  # Déductions possible
+  # ab eafb abd
   # a => c f
   # b => c f
-  # using 4
-  # e => b c d f
-  # a => b c d f
-  # f => b c d f
-  # b => b c d f
-  # using 7
-  # d => a c f
-  # a => a c f
-  # b => a c f
-  # using 8
-  # a => a b c d e f g
-  # b => a b c d e f g
-  # c => a b c d e f g
-  # d => a b c d e f g
-  # e => a b c d e f g
-  # f => a b c d e f g
-  # g => a b c d e f g
+  # c => e g
+  # d => a
+  # e => b d
+  # f => b d
+  # g => e g
 
-  # a => *c* *f* + b *c* d *f* + a *c* *f* + a b *c* d e *f* g => c f
-  # b => *c* *f* + b *c* d *f* + a *c* *f* + a b *c* d e *f* g => c f
-  # c => *
-  # d => a c f + a b c d e f g => a c f
-  # e => b c d f
-  # f => b c d f
-  # g => *
+  # Solution
+  # a => c
+  # b => f
+  # c => g
+  # d => a
+  # e => b
+  # f => d
+  # g => e
 
-  # Valid combinations
-  # abcefg
-  # cf
-  # acdeg
-  # acdfg
-  # bcdf
-  # abdfg
-  # abdefg
-  # acf
-  # abcdefg
-  # abcdfg
-
-  # Normal
-  #   aaaa
-  #  b    c
-  #  b    c
-  #   dddd
-  #  e    f
-  #  e    f
-  #   gggg
-
-  # Input
-  #  dddd
-  # e    a
-  # e    a
-  #  ffff
-  # g    b
-  # g    b
-  #  cccc
-
-  # utiliser partie 1
-
-  def output_sum
-    lines.map do |digits|
-      digits.map do |digit|
-        value = if digit.length == 2
-          "1"
-        elsif digit.length == 4
-          "4"
-        elsif digit.length == 3
-          "7"
-        elsif digit.length == 7
-          "8"
-        else
-          "."
-        end
-
-        puts [digit, value].inspect
+  def output(digits)
+    "abcdefg".split("").permutation.to_a.each do |letters|
+      swaps = "abcdefg".split("").zip(letters).to_h
+      guess = digits.map { _1.gsub(/./, swaps) }.map { _1.split("").sort.join }
+      if guess.all? { VALID_COMBINATIONS.include?(_1) }
+        return guess.map { LETTERS_TO_NUMBERS[_1] }.last(4).map(&:to_s).join.to_i
       end
     end
   end
@@ -136,7 +93,7 @@ RSpec.describe "Day 8" do
       bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
       egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
       gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-      INPUT
+    INPUT
       .split("\n")
   end
   let(:input) { File.read("day8_input.txt").split("\n") }
@@ -150,7 +107,7 @@ RSpec.describe "Day 8" do
       expect(Display.new([example.first]).easy_digit_count).to eql 2
     end
 
-    specify "output_sum", :focus do
+    specify "output_sum" do
       example2 = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
       expect(Display.new([example2]).output_sum).to eql 5353
     end
@@ -165,9 +122,10 @@ RSpec.describe "Day 8" do
   end
 
   specify "part 2 - example" do
-    expect(Display.new(input).output_sum).to eql 61229
+    expect(Display.new(example).output_sum).to eql 61229
   end
 
-  skip "part 2 - answer" do
+  specify "part 2 - answer" do
+    expect(Display.new(input).output_sum).to eql 1010460
   end
 end
